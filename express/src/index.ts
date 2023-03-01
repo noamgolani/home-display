ff4ab338ee32961d0fd914bd9584224f673e736b
noamgolani <noam.golani@gmail.com>
2023-03-01 21:50:26 +0000

    changes

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,10 +1,16 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
-const port = process.env['PORT'] || 4000;
+const port = process.env['PORT'] || 3000;
 
 app.get('/', (_req: Request, res: Response) => {
 	  res.send('Express + TypeScript Server');
+});
+
+app.get('/display', (_req: Request, res: Response) => {
+	  res.send('Display');
 });
 
 app.listen(port, () => {
