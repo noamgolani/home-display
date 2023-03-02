5186c61b016e6594cd96af1418bc3342efee56fa
Noam Golani <noam.golani@gmail.com>
2023-03-02 23:16:19 +0200

    serve public folder

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,18 +1,16 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
-import express, { Express, Request, Response } from 'express';
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
-const port = process.env['PORT'] || 3000;
+const port = process.env["PORT"] || 3000;
 
-app.get('/', (_req: Request, res: Response) => {
-	  res.send('Express + TypeScript Server');
-});
+app.use(express.static("public"));
 
-app.get('/display', (_req: Request, res: Response) => {
-	  res.send('Display');
+app.get("/", (_req: Request, res: Response) => {
+  res.sendFile("public/index.html");
 });
 
 app.listen(port, () => {
-	  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
