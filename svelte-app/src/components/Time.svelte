2b5b590cecf21bccde3f5dfd61111f19dd7a1d31
Noam Golani <noam.golani@gmail.com>
2023-03-02 23:31:54 +0200

    add time and routing + carbon

---
+++ svelte-app/src/components/Time.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let time = new Date();
+
+	$: day = time.getDay();
+	$: month = time.getMonth();
+
+	$: hours = time.getHours();
+	$: minutes = time.getMinutes();
+	$: seconds = time.getSeconds();
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			time = new Date();
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+</script>
+
+<h2>{day}/{month} - {hours}:{minutes}:{seconds}</h2>
--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import 'carbon-components-svelte/css/g90.css';
+	import Time from '../components/Time.svelte';
+</script>
+
+<div class="container">
+	<h1>Home Display</h1>
+	<Time />
+</div>
+
+<style>
+	h1 {
+		font-weight: bold;
+	}
+	.container {
+		padding: 1rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		gap: 1rem;
+	}
+</style>
